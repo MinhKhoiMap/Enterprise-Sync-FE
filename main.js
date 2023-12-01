e92494e20c512abb7b559c3d4c719f99b597ffd6
LilKhang1810 <95366428+LilKhang1810@users.noreply.github.com>
2023-12-01 15:44:17 +0700

    Fix

--- main.js
+++ main.js
@@ -4,22 +4,3 @@ const toggler = document.querySelector(".btn");
 toggler.addEventListener("click",function(){
     document.querySelector("#sidebar")?.classList.toggle("collapsed");
 });
-
-function changeImage(itemId, newSrc) {
-    var item = document.getElementById(itemId);
-    var image = item.querySelector('.icon_nav');
-    image.src = newSrc;
-}
-function selectNavItem(navItemId, selectedImageSrc) {
-
-    var allNavItems = document.getElementsByClassName('nav-item');
-    for (var i = 0; i < allNavItems.length; i++) {
-        var imgElement = allNavItems[i].getElementsByTagName('img')[0];
-        imgElement.src = imgElement.src;
-    }
-
-    
-    var selectedNavItem = document.getElementById(navItemId);
-    var selectedImgElement = selectedNavItem.getElementsByTagName('img')[0];
-    selectedImgElement.src = selectedImageSrc;
-}
