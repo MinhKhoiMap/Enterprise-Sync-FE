05e75120ccedea0971192e87c542796a245988ff
LilKhang1810 <95366428+LilKhang1810@users.noreply.github.com>
2023-12-02 16:22:56 +0700

    Hover Sidebar

--- pages/Sidebar/main.js
+++ pages/Sidebar/main.js
@@ -1,8 +1,10 @@
+// @ts-nocheck
 const navItemList = document.querySelector("#sidebar .sidebar-nav");
 
 console.log([navItemList.children], "navItemList");
 
 for (let child of navItemList.children) {
+  
   child.addEventListener("click", () => {
     for (let child of navItemList.children)
       if (child.classList.contains("nav-item__active")) {
@@ -20,4 +22,26 @@ for (let child of navItemList.children) {
 
     child.classList.add("nav-item__active");
   });
+  child.addEventListener("mouseenter", () => {
+    // Lấy tên của mục
+    let itemName = child.getAttribute("id");
+
+    // Lấy thẻ hình ảnh con của mục
+    const imageChild = child.querySelector(".icon_nav");
+
+    // Đổi hình ảnh khi hover
+    imageChild.setAttribute("src", `../../img/hover/${itemName}_hover.svg`);
+  });
+
+  child.addEventListener("mouseleave", () => {
+    // Lấy tên của mục
+    let itemName = child.getAttribute("id");
+
+    // Lấy thẻ hình ảnh con của mục
+    const imageChild = child.querySelector(".icon_nav");
+
+    // Đổi lại hình ảnh khi rời khỏi
+    imageChild.setAttribute("src", `../../img/${itemName}_click.svg`);
+  });
+
 }
