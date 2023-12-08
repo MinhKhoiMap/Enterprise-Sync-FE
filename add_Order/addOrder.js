b755e9be4291982b761c5835d402110743593125
laphatne280903 <phatnguyen280903@gmail.com>
2023-12-08 17:21:01 +0700

    fiixixiixix loz

--- add_Order/addOrder.js
+++ add_Order/addOrder.js
@@ -1,9 +1,107 @@
 const searchInput = document.getElementById("search-product__input")
+const search_order = [];
 
-console.log([searchInput])
+class Order {
+    total = 0
+    order = []
+    constructor(status_order, order_date, customer_name, customer_address, customer_phone, platform) {
+        this.status_order = status_order;
+        this.order_date = order_date;
+        this.customer_name = customer_name;
+        this.customer_address = customer_address;
+        this.customer_phone = customer_phone;
+        this.platform = platform;
+    }
+
+    addProduct(product) {
+        this.order.push(product)
+        this.total = this.calTotal();
+    }
+
+    removeProduct(id_product) {
+        this.order = this.order.filter((item) => item.id_product !== id_product)
+        this.total = this.calTotal();
+    }
+
+    minusQuantityProduct(id_product, num) {
+        this.order.forEach(item => {
+            if (item.id_product === id_product) {
+                item.quantity -= num
+                if (item.quantity < 1) {
+                    this.removeProduct(id_product)
+                }
+            }
+        })
+        this.total = this.calTotal();
+    }
+
+    plusQuantityProduct(id_product, num) {
+        console.log(id_product)
+        this.order = this.order.map(item => {
+            if (item.id_product === id_product) {
+                console.log()
+                item.quantity += num
+            }
+            console.log(item)
+            return item;
+        })
+        this.total = this.calTotal();
+    }
+
+    calTotal() {
+        this.total = 0;
+        this.order.forEach(item => {
+            console.log(this.total, item.price, item.quantity)
+            this.total += item.price * item.quantity
+        })
+
+        return this.total
+    }
+}
+
+const currentOrder = new Order()
+
+let totalprice_order = document.getElementById("totalPrice");
+
+function addToOrder(id, name, price) {
+    currentOrder.addProduct({ id, name, price, quantity: 1 });
+    const productCart = document.getElementById("search_order")
+
+    currentOrder.order.forEach(({ id, name, price, quantity }) => {
+        console.log({ id, name, price, quantity })
+        let temp = document.createElement("div")
+        temp.setAttribute("class", "d-flex justify-content-between")
+        temp.setAttribute("style", "width: 502px; background-color: white; text-align: center")
+
+        temp.innerHTML = `<p style="width: 110px;">${id}</p>
+        <p style="width: 180px;">${name}</p>
+        <p style="width: 90px;">${price}</p>
+        <div class="quan" style="width:100px; ">
+            <button style="" id="minus" onClick="handleMinusQuantity(event)"><img src="/img/minus.svg"></button>
+            <label id="quantity" style="">${quantity}</label>
+            <button style="" id="plus"><img src="/img/minus.svg"></button>
+        </div>`
+
+        const buttonPlus = temp.querySelector("button#plus")
+        const buttonMinus = temp.querySelector("button#minus")
+
+        buttonPlus.onclick = () => {
+            currentOrder.plusQuantityProduct(id, 1)
+        }
+
+        buttonMinus.onclick = () => {
+            currentOrder.minusQuantityProduct(id, 1)
+        }
+
+
+        productCart.appendChild(temp)
+    });
+
+    totalprice_order.innerText = currentOrder.calTotal()
+}
 
 searchInput.oninput = (e) => {
-    fetch(`https://1d86-171-248-168-162.ngrok-free.app/api/products?product_name=${e.target.value}&platform=Shopee`)
+    fetch(`https://9bf9-14-161-27-27.ngrok-free.app/api/products?product_name=${e.target.value}&platform=Shopee`)
         .then(response => response.json())
         .then((res) => {
             document.getElementById("search_order_res").innerHTML = ""
@@ -23,56 +121,46 @@ searchInput.oninput = (e) => {
                 return quanDiv;
             }
 
-
             const searchProductRes = document.getElementById("search_order_res")
 
             if (res?.data.length > 0) {
-                searchProductRes.innerHTML = res.data.map(({ id_product, product_name, price }) => {
-                    return `
-                    <div class="d-flex justify-content-between" style="background-color: white; text-align:center">
-                        
-                        <p style="width: 502px">${product_name}</p>
-                    </div>
-                    `
-                }).join("");
+                searchProductRes.style.display = "block";
+                // searchProductRes.innerHTML = 
+                res.data.forEach(({ id_product, product_name, price }) => {
+                    let temp = document.createElement("div")
+                    temp.setAttribute("class", "d-flex justify-content-between productDiv")
+                    temp.setAttribute("style", "width: 502px; background-color: white; text-align:center")
+                    temp.innerHTML =
+                        `<p class="id">${id_product}</p>
+                        <p class="product_name">${product_name}</p>
+                        <p class="price">${price}</p>`
 
-                const productBox = document.getElementById("search_order");
+                    temp.onclick = () => {
+                        hideSearchResults()
+                        addToOrder(id_product, product_name, price)
+                    }
 
-                const products = document.querySelectorAll('.product');
-                products.forEach(product => {
-                    product.addEventListener('click', () => {
-                        const selectedProduct = res.data.find(({ id_product }) => id_product === product.id_product);
-                        productBox.innerHTML += `
-                        <div class="d-flex">
-                            <p>${selectedProduct.product_name}</p>
-                            <p>$${selectedProduct.price}</p>
-                        </div>
-                        `;
-                    });
-                });
-
-            } else {
+                    searchProductRes.appendChild(temp);
+                })
+            }
+            else {
                 searchProductRes.innerHTML = "<p class='text-center'>No products found</p>"
             }
 
-            console.log(res)
+            // console.log(res)
         }).catch(err => {
             console.log(err);
         })
-        function hideSearchResults() {
-            document.querySelector("#search_order_res").style.display = "none";
-        }
-        
-        // When a click event occurs on the document, execute the hideSearchResults function
-        document.onclick = hideSearchResults;
-        
-        // Add a click event listener to the search order results
-        document.querySelector("#search_order_res").addEventListener("click", (e) => {
-            // Prevent the hideSearchResults function from executing when clicking on the search order results
-            e.stopPropagation();
-        });
-        
+    function hideSearchResults() {
+        document.querySelector("#search_order_res").style.display = "none";
+    }
+    // When a click event occurs on the document, execute the hideSearchResults function
+    document.onclick = hideSearchResults;
+
+    // Add a click event listener to the search order results
+    document.querySelector("#search_order_res").addEventListener("click", (e) => {
+        // Prevent the hideSearchResults function from executing when clicking on the search order results
+        e.stopPropagation();
+    });
+
 }
-
-
-
